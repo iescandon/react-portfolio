6697b87075cbcefcc553f9c80e5d27e4fe597361
Inez Escandon <iescan4@gmail.com>
2020-09-02 05:14:11 -0500

    added bootstrap and react router dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/navbar';
+import Home from './pages/home';
+import About from './pages/about';
+import Portfolio from './pages/portfolio';
+import Resume from './pages/resume';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div>
+				<Navbar />
+				<Route exact path="/" component={Home} />
+				<Route exact path="/about" component={About} />
+				<Route exact path="/portfolio" component={Portfolio} />
+				<Route exact path="/resume" component={Resume} />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/pages/portfolio.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Portfolio() {
+	return <h1>Porfolio Page</h1>;
+}
+
+export default Portfolio;
