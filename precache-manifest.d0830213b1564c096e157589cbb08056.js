5058eec5bb611d1919123699c3b70a474afb27dc
Inez Escandon <iescan4@gmail.com>
2020-09-02 22:11:12 -0500

    Updates

---
+++ precache-manifest.d0830213b1564c096e157589cbb08056.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cfccc4b6a0203dedceef1d0f9495939",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "494e16bfaf0f97394a22",
+    "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ea1e9d8365fcfe071071",
+    "url": "/react-portfolio/static/css/main.035442d6.chunk.css"
+  },
+  {
+    "revision": "494e16bfaf0f97394a22",
+    "url": "/react-portfolio/static/js/2.4eb73c52.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.4eb73c52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea1e9d8365fcfe071071",
+    "url": "/react-portfolio/static/js/main.2902868c.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "b3843f79c94a129e714bb9166cae79e6",
+    "url": "/react-portfolio/static/media/ask-winston.b3843f79.png"
+  },
+  {
+    "revision": "783ab9240678bb5eeaf50acc6dc7c9ee",
+    "url": "/react-portfolio/static/media/background.783ab924.jpg"
+  },
+  {
+    "revision": "c7deb9458dacae1182639727e510a930",
+    "url": "/react-portfolio/static/media/burger-app.c7deb945.png"
+  },
+  {
+    "revision": "456cae7bc6780d7349dbec1966d62f35",
+    "url": "/react-portfolio/static/media/close-up.456cae7b.jpg"
+  },
+  {
+    "revision": "115c0ac31f3bda2b6c4aacf6e62e1c11",
+    "url": "/react-portfolio/static/media/code-quiz.115c0ac3.png"
+  },
+  {
+    "revision": "e19a9d148a01410c0704eee16e7917e4",
+    "url": "/react-portfolio/static/media/note-taker.e19a9d14.png"
+  },
+  {
+    "revision": "bd53e690f1397f6e6f43ed613545e1d5",
+    "url": "/react-portfolio/static/media/salud.bd53e690.png"
+  },
+  {
+    "revision": "a55322488fbf16f84db2f2fdcb3d8ebf",
+    "url": "/react-portfolio/static/media/weather-dashboard.a5532248.png"
+  }
+]);
